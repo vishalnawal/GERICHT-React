c6a49878a29bb943033584a879152d4c119c4086
vishalnawal <vishalmaheshwari2501@gmail.com>
2023-06-22 21:56:04 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <div className="">
+    Navbar
+  </div>
+);
+
+export default Navbar;
---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="">
+    AboutUs
+  </div>
+);
+
+export default AboutUs;
